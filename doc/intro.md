497e6bf9c2d9be7df8a9373f179b61c0e1ee322f
Karsten Rabe <karstenrabe91@gmail.com>
2020-03-19 23:47:29 -0400

    First clojure project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to starter-project
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/starter_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns starter-project.core-test
+  (:require [clojure.test :refer :all]
+            [starter-project.core :refer :all]))
+
+(deftest canary-test
+  (testing "1 should equal 1"
+    (is (= 1 1))))
